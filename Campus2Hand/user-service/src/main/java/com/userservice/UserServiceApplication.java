c9b833cdc8075cd79d5cf65a13f710e8a0459888
JeropsWang <724636604@qq.com>
2026-06-01 16:59:23 +0800

    依赖添加

--- Campus2Hand/user-service/src/main/java/com/userservice/UserServiceApplication.java
+++ Campus2Hand/user-service/src/main/java/com/userservice/UserServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@MapperScan("com.userservice.mapper")
+
 public class UserServiceApplication {
 
     public static void main(String[] args) {
