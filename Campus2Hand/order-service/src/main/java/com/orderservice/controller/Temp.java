97bc251ed0286064a4cfd7a2cd1345b1ed557e47
周乃浩 <2016897679@qq.com>
2026-06-01 17:36:15 +0800

    订单服务依赖添加

---
+++ Campus2Hand/order-service/src/main/java/com/orderservice/controller/Temp.java
@@ -0,0 +1,15 @@
+package com.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Temp {
+
+    @GetMapping("/temp")
+    public void Hello(){
+        System.out.println("order-service");
+    }
+
+
+}
