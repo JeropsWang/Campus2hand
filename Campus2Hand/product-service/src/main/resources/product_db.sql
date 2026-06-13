79e3a57ebbafde8953c61bcb1d6edbb8a064e168
Guzhi700 <3243127983@qq.com>
2026-06-13 22:05:18 +0800

    商品服务

--- Campus2Hand/product-service/src/main/resources/product_db.sql
+++ Campus2Hand/product-service/src/main/resources/product_db.sql
@@ -5,13 +5,13 @@
  Source Server Type    : MySQL
  Source Server Version : 80041 (8.0.41)
  Source Host           : localhost:3306
- Source Schema         : product
+ Source Schema         : product_db
 
  Target Server Type    : MySQL
  Target Server Version : 80041 (8.0.41)
  File Encoding         : 65001
 
- Date: 11/06/2026 00:44:20
+ Date: 13/06/2026 22:05:02
 */
 
 SET NAMES utf8mb4;
@@ -32,13 +32,18 @@ CREATE TABLE `product`  (
   `publish_time` datetime NULL DEFAULT NULL,
   `status` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of product
 -- ----------------------------
-INSERT INTO `product` VALUES (1, '数码电子', '二手手机', '99新,功能无损坏', 3999.00, '99新', '', '2026-06-08 22:12:51', 'ACTIVE');
-INSERT INTO `product` VALUES (2, '2', '衣服', '全新,功能无损坏', 3999.00, '全新', '', '2026-06-08 22:13:55', 'ACTIVE');
-INSERT INTO `product` VALUES (3, '1', '二手荣耀30pro手机', '89新,功能无损坏，可用正常的使用', 1000.00, '89新', '', '2026-06-08 22:37:36', 'ACTIVE');
+INSERT INTO `product` VALUES (1, '数码电子', '二手手机', '99新,功能无损坏', 3999.00, '99新', '/products/1.jpg', '2026-06-08 22:12:51', 'ACTIVE');
+INSERT INTO `product` VALUES (2, '衣服', '衣服', '全新,功能无损坏', 3999.00, '全新', '', '2026-06-08 22:13:55', 'ACTIVE');
+INSERT INTO `product` VALUES (3, '数码电子', '二手荣耀30pro手机', '89新,功能无损坏，可用正常的使用', 1000.00, '89新', '', '2026-06-08 22:37:36', 'ACTIVE');
+INSERT INTO `product` VALUES (4, '数码电子', '这商品你买不起', '总之如此', 87.00, '95新', '', '2026-06-13 19:18:26', 'ACTIVE');
+INSERT INTO `product` VALUES (5, '数码电子', '这是牢爱', '你想都别想要', 566.97, '7成新及以下', '', '2026-06-13 20:12:36', 'ACTIVE');
+INSERT INTO `product` VALUES (6, '数码电子', '呼呼呼', '235额外tf', 325.00, '99新', '/products/6.jpg', '2026-06-13 20:20:57', 'ACTIVE');
+INSERT INTO `product` VALUES (7, '数码电子', 'eg3q4t54', '23553asrfaw', 63455.00, '99新', '/products/7.jpeg', '2026-06-13 20:33:43', 'ACTIVE');
+INSERT INTO `product` VALUES (8, '数码电子', '我理解了', 'fgswedfA', 322.00, '99新', '/products/8.jpg', '2026-06-13 21:04:46', 'ACTIVE');
 
 SET FOREIGN_KEY_CHECKS = 1;
